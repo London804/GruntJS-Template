35b4e9b432988a4b38a8e09c57ac285b8e0a9660
Alex <alexchaparro@Alexs-MacBook-Pro.local>
2015-03-12 15:40:25 -0400

    uncommented several bootstrap sass files

--- template.js
+++ template.js
@@ -5,7 +5,7 @@ exports.description = 'Create scaffold for all projects';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes =
-'For more information, please see readme.md';
+    'For more information, please see readme.md';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with npm-install ';
@@ -19,22 +19,22 @@ exports.template = function(grunt, init, done) {
     // Copy all files in the root directory
     init.copyAndProcess(files);
 
-        // Generate package.json file, used by npm and grunt.
-        init.writePackageJSON('package.json', {
-            "devDependencies": {
-                "grunt": "^0.4.5",
-                "grunt-contrib-concat": "^0.5.0",
-                "grunt-contrib-jshint": "~0.10.0",
-                "grunt-contrib-nodeunit": "~0.4.1",
-                "grunt-contrib-sass": "^0.8.1",
-                "grunt-contrib-uglify": "^0.6.0",
-                "grunt-contrib-watch": "^0.6.1",
-                "grunt-template": "^0.2.3"
+    // Generate package.json file, used by npm and grunt.
+    init.writePackageJSON('package.json', {
+        "devDependencies": {
+            "grunt": "^0.4.5",
+            "grunt-contrib-concat": "^0.5.0",
+            "grunt-contrib-jshint": "~0.10.0",
+            "grunt-contrib-nodeunit": "~0.4.1",
+            "grunt-contrib-sass": "^0.8.1",
+            "grunt-contrib-uglify": "^0.6.0",
+            "grunt-contrib-watch": "^0.6.1",
+            "grunt-template": "^0.2.3"
 
-            }
-        });
+        }
+    });
 
-        // All done!
-        done();
+    // All done!
+    done();
 
 };
